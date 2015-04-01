b53442af1413cb1a05fc33d9728346abe0220578
Ken Wilcox <wilcoxk@gmail.com>
2015-04-01 15:59:06 -0600

    hide completed tasks

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -5,7 +5,14 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAd: -1}});
+      } else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
     }
   });
 
@@ -19,6 +26,9 @@ if (Meteor.isClient) {
       });
       event.target.text.value = "";
       return false;
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
